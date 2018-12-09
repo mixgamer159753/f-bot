0a1952100e967250d924e37270a9b7e750bfee75
mixgamer159753 <44775482+mixgamer159753@users.noreply.github.com>
2018-12-09 12:54:57 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -12523,5 +12523,31 @@ message.member.addRole(message.guild.roles.find("name", "100"));
 	
 });
 
+client.on('message', function(message) {
+    if(!message.channel.guild) return;
+    if(message.content === 'colors create') {
+    if(message.member.hasPermission('MANAGE_ROLES')) {
+    setInterval(function(){})
+    message.channel.send('يتم انشاء 200 لون انتضر | ▶️')
+    }else{
+    message.channel.send('ما معاك البرمشن المطلوب |❌🚫')
+    }
+    }
+    });
+    
+    client.on('message', message=>{
+    if (message.content === 'colors create'){
+    if(!message.channel.guild) return;
+    if (message.member.hasPermission('MANAGE_ROLES')){
+    setInterval(function(){})
+    let count = 0;
+    let ecount = 0;
+    for(let x = 1; x < 200; x++){
+    message.guild.createRole({name:x,
+    color: 'RANDOM'})
+    }
+    }
+    }
+    });
 
 client.login('NTE1NTkwMTg4MzQ1MzI3NjE2.Dur48Q.bDgggzlD6KcwTK5-e-4Dy3bt-XM');
